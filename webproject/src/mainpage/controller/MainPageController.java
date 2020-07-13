55b97fa9a1b68906e4ec7f3b979d3fa86bc9b94b
goott5-22 <goott5-22@goott5-22-PC>
2020-07-13 17:50:16 +0900

    컨트롤러 업데이트

--- webproject/src/mainpage/controller/MainPageController.java
+++ webproject/src/mainpage/controller/MainPageController.java
@@ -10,27 +10,51 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 @WebServlet("/main")
-public class MainPageController extends HttpServlet{
+public class MainPageController extends HttpServlet {
 
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		
+
 		RequestDispatcher dis = req.getRequestDispatcher("/mainpageview/main.jsp");
 		dis.forward(req, resp);
-		
+
 	} // end doGet()
-	 
+
 	@Override
 	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		
+
 		doProcess(req, resp);
-		
+
 	} // end doPost()
-	
+
 	private void doProcess(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		
-		
-		
+		String path = req.getRequestURI();
+		String next = "";
+
+		HttpSession session = req.getSession();
+		if (session.getAttribute("logOK") == null) {
+			resp.sendRedirect("../login?loginfail=" + path);
+
+			return;
+		}
+		// System.out.println(path);
+		path = path.substring(path.lastIndexOf("/") + 1);
+		if (path.equals("*")) {
+			path = "list.do";
+		}
+		if (path.equals("")) {
+		} else if (path.equals("")) {
+		} else if (path.equals("")) {
+		} else if (path.equals("")) {
+		} else if (path.equals("")) {
+		} else if (path.equals("")) {
+		}
+
+		if (next != "") {
+			RequestDispatcher dis = req.getRequestDispatcher(next);
+			dis.forward(req, resp);
+		}
+
 	} // end doProcess()
-	
+
 } // end class
