8db413cd70c2d40d844cfa998cba661cf1c5725d
m91048911 <68146724+m91048911@users.noreply.github.com>
2020-07-14 19:09:17 +0900

    Add files via upload

---
+++ webproject/src/webproject/templatedbcp/DbcpTemplate.java
@@ -0,0 +1,67 @@
+package templatedbcp;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+import javax.sql.DataSource;
+
+public class DbcpTemplate {
+	public DbcpTemplate() {
+	}
+
+	public static Connection init() throws NamingException, SQLException {
+		Context ct = new InitialContext();
+		DataSource ds = (DataSource) ct.lookup("java:comp/env/jdbc/xe"); // ∞Ì¡§
+		return ds.getConnection();
+	}
+
+	public static void close(Connection conn) {
+		if (conn != null) {
+			try {
+				conn.close();
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+	}// end close()
+
+	public static void close(Statement stmt) {
+		if (stmt != null) {
+			try {
+				stmt.close();
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+	}
+
+	public static void close(PreparedStatement pstmt) {
+		if (pstmt != null) {
+			try {
+				pstmt.close();
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+	}
+
+	public static void close(ResultSet rs) {
+		if (rs != null) {
+			try {
+				rs.close();
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+	}
+}
